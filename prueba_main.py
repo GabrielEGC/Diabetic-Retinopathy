b8d242d72f1e254070155e70547617b77cb09e21
jhairgallardo <jhairgallardo@gmail.com>
2016-11-18 13:23:18 -0500

    Change parameters 100% accuracy (10 images)

--- prueba_main.py
+++ prueba_main.py
@@ -19,14 +19,14 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 from keras import backend as K
 K.set_image_dim_ordering('th')
 
-batch_size = 100   #10
+batch_size = 10   #10
 nb_classes = 2    #5
-nb_epoch = 400    #250
+nb_epoch = 250    #250
 data_augmentation = False
 
-nb_samples=1000 #35123  #20
-nb_train_samples=800#28000 #10
-nb_test_samples=200 #7123  #10
+nb_samples=20 #35123  #20
+nb_train_samples=10#28000 #10
+nb_test_samples=10 #7123  #10
 
 # input image dimensions
 img_rows, img_cols = 256, 256
@@ -72,17 +72,16 @@ print "Split Data..."
 X_train = X_data[0:nb_train_samples]
 Y_train = Y_label[0:nb_train_samples]
 
-#X_test = X_train
-#Y_test = Y_train
-X_test = X_data[nb_train_samples:nb_samples]
-Y_test = Y_label[nb_train_samples:nb_samples]
+X_test = X_train
+Y_test = Y_train
+#X_test = X_data[nb_train_samples:nb_samples]
+#Y_test = Y_label[nb_train_samples:nb_samples]
 
 print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 print "-----------------------------"
-aux_train = aux[0:nb_train_samples]
-aux_test = aux[nb_train_samples:nb_samples]
+
 ###################################################################################
 
 # conviertiendo en vector one hot
@@ -218,8 +217,11 @@ loaded_model.compile(loss='categorical_crossentropy',
 scores = loaded_model.evaluate(X_test, Y_test, verbose=0)
 print("Accuracy loaded model: %.2f%%" % (scores[1]*100))
 ##########################################################################################
+aux_train = aux[0:nb_train_samples]
+#aux_test = aux[nb_train_samples:nb_samples]
+aux_test = aux_train
 
-num_img=2
+num_img=5
 # probando con una imagen
 pyplot.imshow(toimage(X_test[num_img-1]))
 pyplot.title(aux_test[num_img-1])
