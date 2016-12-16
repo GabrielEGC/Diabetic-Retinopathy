dfa64b4a368dab7d35c99309691b8ba64f4c14a7
jhairgallardo <jhairgallardo@gmail.com>
2016-12-16 17:27:04 -0500

    Change graphics 2

--- hist/graficar_hist.py
+++ hist/graficar_hist.py
@@ -6,6 +6,14 @@ train_loss = numpy.load('train_loss.npy')
 test_acc = numpy.load('test_acc.npy')
 test_loss = numpy.load('test_loss.npy')
 
+max_index = numpy.argmax(test_acc)
+test_max_acc = test_acc[max_index]
+loss = test_loss[max_index] 
+
+min_index = numpy.argmin(test_loss)
+test_min_loss = test_loss[min_index]
+acc = test_acc[min_index]
+
 red = 'VGG noFC'
 lr = 0.0001
 decay = 0
@@ -14,11 +22,12 @@ nb_test_samples= 692*2 #2562
 nb_epoch = 100
 
 epocas = []
-for i in range(nb_epoch):
-  epocas.append(i)
+for i in range(1,nb_epoch+1):
+	  epocas.append(i)
 
 pyplot.figure(figsize=(19,12),dpi=100)
-pyplot.suptitle(red + ' - ' +'lr ='+str(lr)+ ' - ' +'[Train Validation]= '+'['+str(nb_train_samples) +' ' +str(nb_test_samples)+']', fontsize="x-large")
+pyplot.suptitle(red + ' - ' +'lr ='+str(lr)+ ' - ' +'[Train Validation]= '+'['+str(nb_train_samples) +' ' +str(nb_test_samples)+']' + ' - ' + 
+				'Max val ACC: %.2f' % (test_max_acc*100) + '%' + ' - ' + ' Epoch: ' + str(max_index+1), fontsize="x-large")
 
 pyplot.subplot(2,1,1)
 pyplot.xlabel('Epoch')
@@ -42,13 +51,7 @@ pyplot.grid()
 
 pyplot.savefig('graficas.png')
 
-max_index = numpy.argmax(test_acc)
-test_max_acc = test_acc[max_index]
-loss = test_loss[max_index] 
 
-min_index = numpy.argmin(test_loss)
-test_min_loss = test_loss[min_index]
-acc = test_acc[min_index]
 print ' '
 print '############ Max val Accuracy ############'
 print 'Max val ACC: ', test_max_acc , ' Epoch: ', max_index+1, ' its loss: ', loss,  ' its train acc: ' ,train_acc[max_index]  , ' its train loss: ', train_loss[max_index] 
