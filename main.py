5f6189e0c13ad542b4f87e601471a07a24eda54b
jhairgallardo <jhairgallardo@gmail.com>
2017-01-16 01:01:28 -0500

    Test 43-2 –  VGG16_noFC – lr 0.00005 – decay 0 – epochs  120 – dist 50/50 – DA con

--- main.py
+++ main.py
@@ -344,8 +344,8 @@ if __name__ == "__main__":
 	nb_classes = 2
 	
 	# TRAIN MODEL INFO	
-	lr = 0.0001
-	decay = 0.00005
+	lr = 0.00005
+	decay = 0
 	momentum = 0.9 
 	nesterov = True
 	batch_size = 64
