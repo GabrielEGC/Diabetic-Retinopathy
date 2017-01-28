5d88e0a6d1aecd491abadf210f04a401b98f54be
jhairgallardo <jhairgallardo@gmail.com>
2017-01-27 20:14:24 -0500

    Test 3 – model 1 – lr 0.1 – decay 0 – epochs 100 – dist 50/50 – DA sin

--- main.py
+++ main.py
@@ -377,20 +377,20 @@ if __name__ == "__main__":
 	nb_classes = 2
 	
 	# TRAIN MODEL INFO	
-	lr = 0.0001
+	lr = 0.1
 	decay = 0
 	momentum = 0.9 
 	nesterov = True
-	batch_size = 64
-	nb_epoch = 80
+	batch_size = 128
+	nb_epoch = 100
 	data_augmentation = True 
-	entrenar = 0
+	entrenar = 1
 
 	print "####################### GET DATA ###############################"
 	(X_train, Y_train, X_val, Y_val, X_test, Y_test) = get_data(img_rows=img_rows, img_cols=img_cols, side=side, same=same, data_per_classes=data_per_classes, nb_classes=nb_classes)
 	input_shape = X_train.shape[1:]
 	print "####################### CREATE MODEL ###########################"
-	model, name = vgg_net(input_shape = input_shape,nb_classes= nb_classes)
+	model, name = model_1(input_shape = input_shape,nb_classes= nb_classes)
 	
 	if entrenar == 1:
 		print "Compile model"
